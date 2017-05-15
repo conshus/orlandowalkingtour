6dca93d032f2fb686645d4c3068630cdaeb212e4
Dwane <Dwane@hiphopandcode.com>
2017-05-15 11:49:14 -0400

    working on getting MaterializeCSS to work with routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import Tours from './Tours';
 import logo from './logo.svg';
 import './App.css';
 import base from './rebase';
 window.base = base; //Use base from console
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 
 class App extends Component {
   render() {
@@ -15,6 +22,36 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul className="collapsible" data-collapsible="accordion">
+          <li>
+            <div className="collapsible-header"><i className="material-icons">filter_drama</i>First</div>
+            <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          </li>
+          <li>
+            <div className="collapsible-header"><i className="material-icons">place</i>Second</div>
+            <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          </li>
+          <li>
+            <div className="collapsible-header"><i className="material-icons">whatshot</i>Third</div>
+            <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          </li>
+        </ul>
+        <Router>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/tours">Tours</Link></li>
+            </ul>
+
+            <hr/>
+
+            {/* <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/> */}
+            <Route path="/tours" component={Tours}/>
+          </div>
+        </Router>
+
       </div>
     );
   }
---
+++ src/Tours.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import base from './rebase';
+window.base = base; //Use base from console
+
+class Tours extends Component {
+  componentDidMount(){
+    $('.collapsible').collapsible();
+  }
+  render() {
+    return (
+      <div className="Tours">
+        <h1>Tours works!</h1>
+
+
+        <ul className="collapsible" data-collapsible="accordion">
+          <li>
+            <div className="collapsible-header"><i className="material-icons">filter_drama</i>First</div>
+            <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          </li>
+          <li>
+            <div className="collapsible-header"><i className="material-icons">place</i>Second</div>
+            <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          </li>
+          <li>
+            <div className="collapsible-header"><i className="material-icons">whatshot</i>Third</div>
+            <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          </li>
+        </ul>
+
+
+
+      </div>
+    )
+  }
+}
+
+export default Tours;
