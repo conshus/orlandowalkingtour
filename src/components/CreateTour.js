5429fd857e3d2b3b95e939bebfe882e310a21efe
Dwane <Dwane@hiphopandcode.com>
2017-05-17 11:22:21 -0400

    got all locations to list

--- src/components/CreateTour.js
+++ src/components/CreateTour.js
@@ -2,9 +2,50 @@ import React, { Component } from 'react';
 import UserMenu from './UserMenu';
 import Map from './Map';
 import Locations from './Locations'
+import base from '../rebase';
+window.base = base; //Use base from console
 
 class CreateTour extends Component {
+  constructor (){
+    super();
+    this.state = {
+      user: {},
+      locations:[],
+      tours:[]
+    }
+  }
+
+  componentDidMount() {
+    base.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log('User is signed in.', user);
+        this.setState({
+          user: user
+        })
+        base.syncState(`/historic-locations/orlando`,{
+          context: this,
+          state: 'locations',
+          asArray: true
+        })
+        base.syncState(`/users/${user.uid}/tours`,{
+          context: this,
+          state: 'tours',
+          asArray: true
+        })
+      } else {
+        console.log('User is logged out.')
+        this.setState({
+          user: {}
+        })
+       }
+    });
+
+  }
+
+
   render() {
+    {console.log(this.state.locations)}
+
     return (
       <div className="CreateTour">
         <UserMenu />
@@ -15,7 +56,7 @@ class CreateTour extends Component {
               mapElement={<div id='mapElement' />}
             />
           </div>
-          <div className="col s12 m6"><Locations /></div>
+          <div className="col s12 m6"><Locations locations={this.state.locations} /></div>
         </div>
       </div>
     )
--- src/components/Locations.js
+++ src/components/Locations.js
@@ -8,17 +8,33 @@ class Locations extends Component {
     window.$ = window.jQuery;
     $('ul.tabs').tabs();
   }
+  displayAllLocations(){
+    return(
+      <div>
+        <div id="allLocations" className="col s12">
+          <ul className="collection">
+            {this.props.locations.map((location, index) => {
+              return(
+                <li className="collection-item" key={index}><div>{location.name}<a href="#" className="secondary-content"><i className="material-icons">send</i></a></div></li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    )
+  }
   render() {
+    console.log(this.props.locations)
     return (
       <div className="Locations">
         <div className="row">
           <div className="col s12">
             <ul className="tabs tabs-fixed-width">
-              <li className="tab col s3"><a className="active" href="#test1">All Locations</a></li>
+              <li className="tab col s3"><a className="active" href="#allLocations">All Locations</a></li>
               <li className="tab col s3"><a href="#test2">Selected Locations</a></li>
             </ul>
           </div>
-          <div id="test1" className="col s12">List of all locations</div>
+          {this.displayAllLocations()}
           <div id="test2" className="col s12">Selected Locations</div>
         </div>
       </div>
