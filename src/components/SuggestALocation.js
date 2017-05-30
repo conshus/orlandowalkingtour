39a04e4b5638084d8381a10243440abe133326f2
Dwane <Dwane@hiphopandcode.com>
2017-05-30 18:08:31 -0400

    worked on styling for add photo section

--- src/components/SuggestALocation.js
+++ src/components/SuggestALocation.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
-import UserMenu from './UserMenu'
+import UserMenu from './UserMenu';
+import canUseDOM from "can-use-dom";
+
+//Get current location
+const geolocation = (
+  canUseDOM && navigator.geolocation ?
+  navigator.geolocation :
+  ({
+    getCurrentPosition(success, failure) {
+      failure(`Your browser doesn't support geolocation.`);
+    },
+  })
+);
 
 class SuggestALocation extends Component {
   render(){
@@ -19,7 +31,40 @@ class SuggestALocation extends Component {
               <div className="card-content">
                 <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
                 <p><a href="#">This is a link</a></p>
-                <input type="file" accept="image/*" capture="camera" />
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input id="location_name" type="text" className="validate" />
+                    <label htmlFor="location_name">Location Name</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12 m9">
+                    <input id="location_address" type="text" className="validate" />
+                    <label htmlFor="location_address">Location Address</label>
+                  </div>
+                  <div className="col s12 m3">
+                    <a className="waves-effect waves-light btn-large"><i className="material-icons left">cloud</i>Locate</a>
+                  </div>
+                </div>
+                <form>
+                  <div className="file-field input-field">
+                    <div className="btn">
+                      <span>Photo</span>
+                      <input type="file" accept="image/*" capture="camera" />
+                    </div>
+                    <div className="file-path-wrapper">
+                      <input className="file-path validate" type="text" />
+                    </div>
+                  </div>
+                </form>
+
+                <div className="row">
+                  <div className="input-field col s12">
+                    <textarea id="reason" className="materialize-textarea"></textarea>
+                    <label htmlFor="reason">Textarea</label>
+                  </div>
+                </div>
+
               </div>
               <div className="card-reveal">
                 <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
